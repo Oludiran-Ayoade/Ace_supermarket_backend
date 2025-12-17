9f94919525e32aad1be084b2f6b0db9e305cd3d6
Oludiran-Ayoade <Gracegold@Oludiran-Ayoades-MacBook-Air.local>
2025-12-17 11:00:24 +0100

    Fix SSL connection for Render PostgreSQL

--- config/database.go
+++ config/database.go
@@ -19,8 +19,14 @@ func ConnectDatabase() error {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	// Use SSL for production (Render), disable for local development
+	sslmode := "disable"
+	if host != "localhost" && host != "127.0.0.1" {
+		sslmode = "require"
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
 
 	var err error
 	DB, err = sql.Open("postgres", connStr)
